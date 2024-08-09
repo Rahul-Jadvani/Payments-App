96c2a2627743e9fdfa72779fecaac71d614ab330
Ray <rahuljadvani24@gmail.com>
2024-08-09 15:22:41 +0530

    fixed icon

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -3,12 +3,15 @@ import Signup from './routes/signup';
 import Signin from './routes/signin';
 import Dashboard from './routes/dashboard';
 import Transaction from './routes/transaction';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Redirect root path to signup */}
+        <Route path="/" element={<Navigate to="/signup" />} />
+
         <Route path='/signup' element={<Signup />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/dashboard' element={<Dashboard />} />
